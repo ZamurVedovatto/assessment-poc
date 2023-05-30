3b23e842443b84a94ddd76be2a1dc3c668b8b50c
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-05-30 19:59:52 -0300

    quiz reducer and initial state

--- quiz-project/src/components/Welcome.tsx
+++ quiz-project/src/components/Welcome.tsx
@@ -5,7 +5,7 @@ import Quiz from "../img/quiz.svg";
 import "./Welcome.css";
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
   console.log("quizState", quizState)
 
@@ -13,7 +13,7 @@ const Welcome = () => {
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button onClick={() => console.log('')}>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>
         Iniciar
       </button>
       <img src={Quiz} alt="Início do Quiz" />
