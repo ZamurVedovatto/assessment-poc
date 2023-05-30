c3d98975639a047f00bb0b9538fbc01ec2ab68a5
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-05-29 21:21:16 -0300

    improve useFetch hook

--- react-api-requests/api-requests/src/hooks/useFetch.ts
+++ react-api-requests/api-requests/src/hooks/useFetch.ts
@@ -1,16 +1,27 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
+const api = axios.create({
+    baseURL: 'https://api.github.com'
+})
 
-export function useFetch<T = unknown>(url: string) {
-    const [data, setData] = useState<T | null>(null)
+export function useFetch<T = unknown>(url: string, options?: AxiosRequestConfig) {
+    const [data, setData] = useState<T | null>(null);
+    const [isFetching, setIsFetching] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        axios.get(url)
+        api.get(url, options)
             .then(response => {
                 setData(response.data)
             })
+            .catch((error) => {
+                setError(error)
+            })
+            .finally(() => {
+                setIsFetching(false)
+            })
     }, [])
 
-    return { data }
+    return { data, error, isFetching }
 }
