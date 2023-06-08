0b56f915133682477094b1227128d51cd274b8af
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-06-08 19:36:35 -0300

    useState react hook

--- typescript-react/src/Components/Person.tsx
+++ typescript-react/src/Components/Person.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, ChangeEvent, useState } from 'react'
 
 interface Props {
     name: string;
@@ -9,12 +9,23 @@ interface Props {
 
 const Person: FC<Props> = ({name, age, email}) => {
 // const Person = ({name, age, email}: Props) => {
+    const [country, setCountry] = useState<string | null>(null);
+
+    const handleChange = (event: ChangeEvent<HTMLInputEvent>) => {
+        setCountry(event.target.value)
+    }
 
     return (
         <>
             <h1>{name}</h1>
             <h1>{age}</h1>
             <h1>{email}</h1>
+            <input
+                placeholder="Write down your Country.."
+                // onChange={(e: React.ChangeEvent<HTMLInputEvent>) => setCountry(e.target.value)}
+                onChange={handleChange}
+                />
+            <span>{country}</span>
         </>
     )
 }
