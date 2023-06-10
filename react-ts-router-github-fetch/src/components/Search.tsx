275bac21c8d0eac075ca78b00a71d32c547dff5f
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-06-10 14:08:58 -0300

    set css via css modules

--- react-ts-router-github-fetch/src/components/Search.tsx
+++ react-ts-router-github-fetch/src/components/Search.tsx
@@ -1,14 +1,15 @@
 import { FC, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import { SearchProps } from "./../types/Search.ts";
+import classes from "./Search.module.css";
 
 const Search: FC<SearchProps> = ({ loadUser }) => {
   const [userName, setUserName] = useState("");
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Search by User</h2>
       <p>Know the best repos</p>
-      <div>
+      <div className={classes.search_container}>
         <input
           type="text"
           placeholder="Type the username"
