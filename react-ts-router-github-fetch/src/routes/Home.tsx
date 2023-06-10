e0e3d6eed119f3e4f567f3483bb2776241ba55be
Zamur Borges Vedovatto <vedovatto@gmail.com>
2023-06-10 14:30:36 -0300

    set Error component

--- react-ts-router-github-fetch/src/routes/Home.tsx
+++ react-ts-router-github-fetch/src/routes/Home.tsx
@@ -1,15 +1,26 @@
 import Search from "./../components/Search";
 import User from "./../components/User";
+import Error from "./../components/Error";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { UserProps } from "./../types/User.ts";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
+
     const { avatar_url, login, location, followers, following } = data;
     const userData: UserProps = {
       avatar_url,
@@ -27,6 +38,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
